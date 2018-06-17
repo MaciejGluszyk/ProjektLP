9eff27b70c3ac03615e65c001ec69629399998b5
MaciejGluszyk <37669670+MaciejGluszyk@users.noreply.github.com>
2018-06-17 23:01:48 +0200

    Admin shop

---
+++ ProjektLP/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using ProjektLP.Models.Data;
+using ProjektLP.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjektLP.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
+        {
+            //Declare a list of models
+            List<CategoryVM> categoryVMList;
+            using (Db db = new Db())
+            {
+
+
+                //Init the list
+                categoryVMList = db.Categories
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new CategoryVM(x))
+                    .ToList();
+            }
+            //Return view with list
+            return View(categoryVMList);
+        }
+    }
+}

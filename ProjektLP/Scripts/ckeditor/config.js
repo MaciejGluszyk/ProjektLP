e43a0b4b78674fd983346f7ec67e58ae06a3a657
MaciejGluszyk <37669670+MaciejGluszyk@users.noreply.github.com>
2018-06-17 21:53:59 +0200

    Admin finished

--- ProjektLP/Scripts/ckeditor/config.js
+++ ProjektLP/Scripts/ckeditor/config.js
@@ -9,5 +9,8 @@ CKEDITOR.editorConfig = function( config )
 	// config.language = 'fr';
     // config.uiColor = '#AADC6E';
 
-    
+    var roxyFileman = '/fileman/index.html?integration=ckeditor';
+    config.filebrowserBrowseUrl = roxyFileman;
+    config.filebrowserImageBrowseUrl = roxyFileman + '&type=image';
+    config.removeDialogTabs = 'link:upload;image:upload';
 };
